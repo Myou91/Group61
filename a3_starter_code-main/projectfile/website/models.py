e3f65566f7f2ae1f9660c350dbe02284806139ba
n11348186 <147817077+n11348186@users.noreply.github.com>
2023-10-20 19:58:19 +1000

    Update models.py
    Added extra row in database table to meet design requirement

--- a3_starter_code-main/projectfile/website/models.py
+++ a3_starter_code-main/projectfile/website/models.py
@@ -42,6 +42,25 @@ class Events(db.Model):
     comments = db.relationship('Comment', backref='events')
     # relation to call events.user and event.created_by
     user = db.relationship('User', backref='events')
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    description = db.Column(db.String(1024))
+    address = db.Column(db.String(300))
+    date = db.Column(db.Date, nullable=False)
+    #Proj Req 1.  event by category 
+    category = db.Column(db.Integer, default=0)
+    start_time = db.Column(db.Time, nullable=False)
+    finish_time = db.Column(db.Time, nullable=False)
+    price = db.Column(db.Numeric(5,2), nullable=False)
+    ticket_max = db.Column(db.Integer)
+    ticket_remain = db.Column(db.Integer)
+    #boolean Event cancelled
+    cancel = db.Column(db.Boolean, default=False, nullable=False)
+    image = db.Column(db.String(400))
+    comments = db.relationship('Comment', backref='event')
     
     
 
@@ -62,6 +81,20 @@ class Comment(db.Model):
     def __repr__(self):
         return f"Name: {self.name}"
     
+class Booking(db.Model):
+    __tablename__ = 'bookings'
+    id = db.Column(db.Integer, primary_key=True)
+    ticket = db.Column(db.Integer)
+    paymentdate = db.Column(db.DateTime, default=datetime.now())
+    amount = db.Column(db.Numeric(10,2))
+    #No order confirm because when giving out bookingID is already confirm
+    reference = db.Column(db.String(6))
+    consume = db.Column(db.Boolean, default=False, nullable=False)
+    refund = db.Column(db.Boolean, default=False, nullable=False)
+    # add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+
 
 class Bookings(db.Model):
     __tablename__ = 'bookings'
