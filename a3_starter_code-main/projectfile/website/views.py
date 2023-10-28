6235aba3d5d57d6fd27b44e5216a756cd208e2ce
n11348186 <147817077+n11348186@users.noreply.github.com>
2023-10-28 11:39:35 +1000

    Update views.py
    Added my  code to the project

--- a3_starter_code-main/projectfile/website/views.py
+++ a3_starter_code-main/projectfile/website/views.py
@@ -1,7 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Event
+from . import db
 
-main_bp = Blueprint('main', __name__)
+mainbp = Blueprint('main', __name__)
 
-@main_bp.route('/')
+@mainbp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    events = db.session.scalars(db.select(Event)).all()    
+    return render_template('index.html', events=events)
+
+@mainbp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        events = db.session.scalars(db.select(Event).where(Event.description.like(query)))
+        return render_template('index.html', events=events)
+    else:
+        return redirect(url_for('main.index'))
+
+
+@mainbp.route('/age-group')
+def age_group(category):
+    age_groups = {
+        1: 'Age group 0-3',
+        2: 'Age group 4-8',
+        3: 'Age group 9-16'
+    }
+
+    if category in age_groups:
+        return age_groups[category]
+    else:
+        return 'Invalid index'
