c2b1e48627c48f0a5409b96dd5e959d4619c6024
Zeta Choi <147817077+n11348186@users.noreply.github.com>
2023-10-31 20:40:45 +1000

    Update events.py
    Fail to read user id in booking section

--- a3_starter_code-main/projectfile/website/events.py
+++ a3_starter_code-main/projectfile/website/events.py
@@ -6,7 +6,7 @@ import os
 from werkzeug.utils import secure_filename
 #additional import:
 from flask_login import login_required, current_user
-import random, string
+import random, string, secrets
 
 eventbp = Blueprint('event', __name__, url_prefix='/events')
 
@@ -79,7 +79,7 @@ def comment(id):
 #Random string generator
 def gen_random_string():
     letters_and_digits = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters_and_digits) for i in range(6))
+    return ''.join(secrets.choice(letters_and_digits) for _ in range(6))
 
 
 #Handle Event booking field in Event detail page
@@ -89,27 +89,27 @@ def booking(id):
     form = BookingForm()  
     #get the event object associated to the page and the comment
     event = db.session.scalar(db.select(Event).where(Event.id==id))
-    print(event)
-    print(event.ticket_remain)
-    print(form.ticket.data)
     if form.validate_on_submit():  
       #System rules no user book more than 6 ticket
-      if form.ticket.data > 6:
+      if  form.ticket.data > 6 and form.ticket.data < 1:
         flash('Ticket request is more than 6', 'error')
       else:
-         print("pass ticket is less than 6 check")
          #Handle ticket remain not enough to user request
          if form.ticket.data > event.ticket_remain:
            flash('Ticket request is more than Available', 'warning')
          else:
             #Create booking for user and update event.ticket_remain
-            #read the comment from the form
-            print("save value now")
+            #read the ticket request from the form
             event.ticket_remain -= form.ticket.data
             bprice = event.price * form.ticket.data
-
-            booking = Booking(ticket=form.ticket.data, reference = gen_random_string(), event=event,
-                        user=current_user, amount = bprice)          
+            print(event.ticket_remain)
+            print(bprice, id)
+            print(current_user)
+            #Call function to update event.ticket_remain after ticket sold
+            update_event_ticketsold(id, event.ticket_remain)
+            booking = Booking(ticket = form.ticket.data, reference = 'abc123', event_id=event.id,
+                        user_id='2', amount = bprice) 
+            print(booking)         
             #here the back-referencing works - comment.event is set
             # and the link is created
             db.session.add(booking) 
@@ -119,3 +119,8 @@ def booking(id):
             # print('Your comment has been added', 'success') 
           # using redirect sends a GET request to event.show
       return redirect(url_for('event.show', id=id))
+    
+def update_event_ticketsold(id, sold):
+    event = db.session.scalar(db.select(Event).where(Event.id==id))
+    event.ticket_remain = sold
+    db.session.commit() 
